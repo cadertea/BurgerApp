0ccd650f04cdeb141db11f28c127d46f52ec4429
Cadertea <cadertea@ncsu.edu>
2020-12-03 11:03:34 -0500

    updated the seeds and scheme, aswell as created connections and orms

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Derteano007!",
+    database: "burgers_db"
+  });
+
+// ===== MAKING CONNECTION =====
+connection.connect(function (err) {
+  if (err) {
+    console.error("connection error: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+//  ===== EXPORT CONNECTION FOR OUR ORM TO USE =====
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+  
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mojoes Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDonalds Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BurgerKing Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('OverUnder Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('DoubleCheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
